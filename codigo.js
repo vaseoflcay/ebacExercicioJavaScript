fc738bf58296181b728a54010315ed8a2304f103
vaseoflcay <75212961+vaseoflcay@users.noreply.github.com>
2025-03-17 09:19:52 -0400

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -7,22 +7,31 @@ const MDC = (a,b) => {
     return a;
 }
 
-const Multiplos = (c,d) => {
+var lista_numeros=[1,65,3,4,-5,12,245,32,12,3,6];
+        Array.min = function(array) {
+            return Math.min.apply(Math, array);
+        }
+        
+        Array.max = function(array) {
+            return Math.max.apply(Math, array);
+        };
+        console.log(Array.min(lista_numeros));
+        console.log(Array.max(lista_numeros));
 
-    let resposta = 0 
+
+
+const Multiplos = (c,d) => {
+    
     let numero = 1
     let soma=0
-    
-    
-    while (numero <= 1000) {
-    if (numero % c === 0 && numero % d ===0) {
+        
+    while (numero < 1000) {
+    if (numero % c === 0 || numero % d ===0) {
     soma=soma + numero
-    } 
+     } 
     numero +=1
-    }
-    
+     }
 return soma;
-
 
 }
 
